8454cf119a5919d4a7cf0492e2894a3c51f5c115
Remigiusz Orlowski <remigiusz.orlowski@gmail.com>
2021-07-27 15:08:23 +0200

    Add delay simulation for nice live-like async demo

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,19 @@ const bodyParser = require('body-parser')
 const Utils = require('./utils');
 app.use(bodyParser.json())
 
+const { DELAY_SIMULATE: delaySimulate } = process.env;
+
+app.use(async (req, res, next) => {
+    if (delaySimulate) await new Promise(r => setTimeout(r, Number(delaySimulate)));
+    next();
+});
+
 function serve() {
     app.get('/ohms/:id', async (req, res) => {
         const ohm = await Utils.getOhmById(req.params.id);
         if (!ohm) res.status(404).send({ message: 'Item not found!' }).end();
         else res.send(ohm);
-    })
-
+    });
     app.listen(3000, () => console.log('listening on port 3000'));
 }
 
